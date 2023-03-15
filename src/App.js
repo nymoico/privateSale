fcb19f2feaef370d32b81a467b3a471f1547ea64
Jarmo van de Seijp <dewebbutler@gmail.com>
2023-03-15 17:36:08 +0100

    update repo

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+<<<<<<< HEAD
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { EthereumClient,w3mConnectors, w3mProvider } from "@web3modal/ethereum";
+import {bsc,bscTestnet} from 'wagmi/chains'
+=======
 import { configureChains, createClient } from "wagmi";
 import { EthereumClient,w3mConnectors, w3mProvider } from "@web3modal/ethereum";
 import { goerli } from "wagmi";
 import {bsc} from 'wagmi/chains'
+>>>>>>> 97a14d09b91183f98852124984a55169c209cfd9
 import { Web3Modal } from "@web3modal/react";
 
 // Configure chains & providers with the Alchemy provider.
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
 const projectID = '775c78504d00b798fba3744c24ca8f2e'
 
+<<<<<<< HEAD
+const {provider} = configureChains([bsc],[w3mProvider({projectId:projectID})])
+const wagmiClient = createClient({
+  autoConnect:true,
+  connectors:w3mConnectors({projectId:projectID,version:1,chains:[bsc]}),
+=======
 const {provider} = configureChains([goerli],[w3mProvider({projectID})])
 const wagmiClient = createClient({
   autoConnect:true,
   connectors:w3mConnectors({projectID,version:1,bsc}),
+>>>>>>> 97a14d09b91183f98852124984a55169c209cfd9
   provider
 })
 const ethereumClient = new EthereumClient(wagmiClient,[bsc])
@@ -37,7 +50,11 @@ function App() {
 
   return (
     <>
+<<<<<<< HEAD
+    <WagmiConfig client={wagmiClient}>
+=======
     <wagmiClient client={wagmiClient}>
+>>>>>>> 97a14d09b91183f98852124984a55169c209cfd9
     <BrowserRouter>
       <Navbar />
       <Routes>
@@ -45,7 +62,11 @@ function App() {
       </Routes>
       <Footer />
     </BrowserRouter>
+<<<<<<< HEAD
+    </WagmiConfig>
+=======
     </wagmiClient>
+>>>>>>> 97a14d09b91183f98852124984a55169c209cfd9
     <Web3Modal projectId={projectID} ethereumClient={ethereumClient}/>
     </>
   );
--- src/components/Purchase/EthCard.js
+++ src/components/Purchase/EthCard.js
@@ -1,12 +1,68 @@
 import React, { useEffect, useState } from "react";
 import styles from "./styles.module.css";
+import { ethers } from "ethers";
+import { ICOADDRESS, ICOABI } from "../../contractUtils/contractConfig";
+import { useBalance, useAccount, useSigner, usePrepareContractWrite, useContractWrite } from 'wagmi'
+
 
 const EthCard = () => {
-  const [eth, setEth] = useState(0);
+  const [eth, setEth] = useState(100);
   const [ethNymo, setEthNymo] = useState(0);
-  useEffect(() => {
-    setEthNymo(eth * 90487.65);
-  }, [eth]);
+  const [bnbPrice, setBnbPrice] =useState(0);
+  const [nymoAmount, setNymoAmount] = useState(0);
+  const [callAmount, setCallAmount] = useState(0);
+
+  const { isConnected, address } = useAccount();
+  
+  const balance = useBalance({
+    address: address,
+  });
+    
+  useEffect(()=>{
+
+    async function fetchEthPrice() {
+    const provider = new ethers.providers.JsonRpcProvider('https://billowing-proud-butterfly.bsc.discover.quiknode.pro/c0cbec7e2023692be00972bc02837556798db23d/')
+    const aggregatorV3InterfaceABI = [{ "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "description", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint80", "name": "_roundId", "type": "uint80" }], "name": "getRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "latestRoundData", "outputs": [{ "internalType": "uint80", "name": "roundId", "type": "uint80" }, { "internalType": "int256", "name": "answer", "type": "int256" }, { "internalType": "uint256", "name": "startedAt", "type": "uint256" }, { "internalType": "uint256", "name": "updatedAt", "type": "uint256" }, { "internalType": "uint80", "name": "answeredInRound", "type": "uint80" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "version", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }]
+    const addr = "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE"
+    const priceFeed = new ethers.Contract(addr, aggregatorV3InterfaceABI, provider)
+    const price = await priceFeed.latestRoundData();
+
+    console.log(Number((price.answer / Math.pow(10, 8))));
+    setBnbPrice(Number((price.answer / Math.pow(10, 8))));
+    }
+    
+    fetchEthPrice();        
+
+      
+  },[])
+
+  const handleChange = async(e) => {
+    console.log(e)
+    setEth(e.target.value);
+    const provider = new ethers.providers.JsonRpcProvider('https://billowing-proud-butterfly.bsc.discover.quiknode.pro/c0cbec7e2023692be00972bc02837556798db23d/')
+    const contract = new ethers.Contract(ICOADDRESS,ICOABI,provider);
+    const results = await contract.bnbBuyHelper(e.target.value);
+    console.log(typeof ethers.utils.formatUnits(results))
+    setCallAmount(results);
+    setEthNymo(parseFloat((ethers.utils.formatUnits(results))).toFixed(6));
+
+  }
+
+
+  const { data: signer, isError, } = useSigner()
+
+    
+  async function callContract(){
+
+      console.log(signer)
+      try{
+    const contract = new ethers.Contract(ICOADDRESS,ICOABI,signer)
+    const  tx = contract.buyWithBNB(eth,{value: callAmount})
+    } catch(e){
+      console.log(e)
+    }
+    }
+
 
   return (
     <div
@@ -21,6 +77,18 @@ const EthCard = () => {
             <input
               className={styles.input}
               type="number"
+<<<<<<< HEAD
+              min="100"
+              value={eth}
+              onChange={
+                handleChange
+              }
+            />{" "}
+            <p className={styles.label}> NYMO</p>
+          </div>{" "}
+        </div>
+        <p className={styles.value}>{ethNymo} BNB</p>
+=======
               min="0"
               value={eth}
               onChange={(e) => {
@@ -31,6 +99,7 @@ const EthCard = () => {
           </div>{" "}
         </div>
         <p className={styles.value}>{ethNymo} NYMO</p>
+>>>>>>> 97a14d09b91183f98852124984a55169c209cfd9
         <div className={`${styles.inputWrapper}`}>
           <div className={`${styles.inputContainer}`}>
             <input
@@ -38,17 +107,35 @@ const EthCard = () => {
               min="0"
               type="number"
               value={ethNymo}
+<<<<<<< HEAD
+              disabled="true"
+            />{" "}
+            <p className={styles.label}> BNB</p>
+=======
               onChange={(e) => setEthNymo(e.target.value)}
             />{" "}
             <p className={styles.label}> NYMO</p>
+>>>>>>> 97a14d09b91183f98852124984a55169c209cfd9
           </div>{" "}
         </div>
 
         <p className={styles.value}>
+<<<<<<< HEAD
+          {eth} NYMO = {eth * 0.0008} USDT
+        </p>
+      </div>{" "}
+      <div>
+      <button className={`${styles.button} ${styles.buyButton} `} onClick={callContract}>
+        buy
+      </button>
+      
+    </div>
+=======
           {ethNymo} NYMO = {eth} BNB
         </p>
       </div>{" "}
       <button className={`${styles.button} ${styles.buyButton} `}>Buy</button>
+>>>>>>> 97a14d09b91183f98852124984a55169c209cfd9
     </div>
   );
 };
