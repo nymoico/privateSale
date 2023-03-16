42f947724c797c367b93049a01c910f1a05822c6
Jarmo van de Seijp <dewebbutler@gmail.com>
2023-03-16 18:19:35 +0100

    refactor card

--- src/components/Purchase/UsdtCard.js
+++ src/components/Purchase/UsdtCard.js
@@ -16,18 +16,19 @@ const UsdtCard = () => {
     >
       <h4 className={styles.cardTitle}>Buy With $USDT</h4>
       
-        <h5 className={styles.value}>Disabled</h5>
+        <h5 style={{fontFamily:"Lato"}} className={styles.value}>Disabled</h5>
+        <br/>
         <div style ={{display:"flex", justifyContent:"center"}}>
-        <p style ={{marginLeft:"25px"}}className={styles.value}>
+        <p  style ={{marginLeft:"25px", fontFamily:"Lato"}}className={styles.value}>
           Due to the depegging of USDC, we are currently not accepting oracleized stable assets. 
           To use our manual converter, please send assets other than BNB to :
         </p>
         </div>
         <br />
-        <h6 style ={{marginLeft:"25px"}} >0xA360991E332778c4549Ab657a1Cf1fb3d198F173</h6>
+        <h5 style ={{marginLeft:"25px"}} >0xA360991E332778c4549Ab657a1Cf1fb3d198F173</h5>
         <br />
         
-        <p  style ={{marginLeft:"25px"}} className={styles.value}>
+        <p  style ={{marginLeft:"25px", fontFamily:"Lato"}} className={styles.value}>
           We will manually convert them to NYMO.
         </p>
       </div>
