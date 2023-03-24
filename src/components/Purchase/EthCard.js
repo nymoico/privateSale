3e1bd72493717c87c1c4fa19fa78a0439cbf2d7d
Jarmo van de Seijp <dewebbutler@gmail.com>
2023-03-24 10:30:33 +0100

    lower fees

--- src/components/Purchase/EthCard.js
+++ src/components/Purchase/EthCard.js
@@ -76,7 +76,7 @@ const EthCard = () => {
         try{
         console.log(eth);
         const contract = new ethers.Contract(ICOADDRESS,ICOABI,signer);
-        const amount = Math.floor(eth*1.05);
+        const amount = Math.floor(eth*1.02);
         console.log(amount);
         console.log(typeof amount);
         const callAmount = await contract.bnbBuyHelper(amount);
